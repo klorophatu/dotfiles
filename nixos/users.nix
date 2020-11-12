c4b47c9dc8e176112899f8cf1521ea811773c448
klorophatu <klorophatu@krutt.org>
2020-11-12 16:01:30 +0200

    Remove nix related files

--- nixos/users.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-{
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.eon = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.zsh;
-  };
-}
